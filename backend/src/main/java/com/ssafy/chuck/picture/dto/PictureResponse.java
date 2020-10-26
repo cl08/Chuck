76aa757f5493456d6f48db809522a2fbc289a294
average10@naver.com <average10@naver.com>
2020-10-26 17:59:23 +0900

    [feature] S03P31A206-130 : PictureResponse 구현

---
+++ backend/src/main/java/com/ssafy/chuck/picture/dto/PictureResponse.java
@@ -0,0 +1,11 @@
+package com.ssafy.chuck.picture.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class PictureResponse {
+	private int diary_id;
+	private List<String> path_list;
+}
