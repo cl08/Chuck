1b0459c5afd60e733a1b7fbac650b4cec3bf618d
average10@naver.com <average10@naver.com>
2020-10-27 18:29:28 +0900

    [feature] S03P31A206-141 : flask 서버 생성 및 Spring Boot와의 통신 API 작성

---
+++ backend/python/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import request
+from my_clustering import cluster, clustering
+# from urllib.parse import quote # url 한글 인코딩 Error 해결
+
+app = Flask(__name__)
+
+@app.route('/cluster')
+def cluster():
+    groupId = request.args.get('groupId', 'groupId')
+    print(groupId)
+    return clustering(groupId)
+
+if __name__ == '__main__':
+    app.run()
