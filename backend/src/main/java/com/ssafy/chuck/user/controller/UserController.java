5484461aa544f68495081b869ddda42d82443771
kickrisk <dsghdh123@naver.com>
2020-10-28 17:45:48 +0900

    [bug] Swagger Bug fix

--- backend/src/main/java/com/ssafy/chuck/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/chuck/user/controller/UserController.java
@@ -39,7 +39,7 @@ public class UserController {
 	@RequestMapping(method = RequestMethod.POST, produces = "application/json")
 	@ApiOperation(value = "로그인 또는 회원가입", notes = "카카오 API 통해 받은 토큰을 기반으로 신규회원이라면 DB에 저장하는 메서드",
 		response = UserDto.class)
-	@ApiImplicitParam(name = "accessToken", value = "Kakao API를 통해서 받는 임의의 토큰 값", required = true)
+	// @ApiImplicitParam(name = "accessToken", value = "Kakao API를 통해서 받는 임의의 토큰 값", required = true)
 	@ApiResponses({
 		@ApiResponse(code = 200, message = "로그인 또는 회원가입 성공"),
 		@ApiResponse(code = 400, message = "잘못된 요청입니다"),
@@ -63,9 +63,9 @@ public class UserController {
 	@RequestMapping(method = RequestMethod.GET, value = "/{userId}", produces = "application/json")
 	@ApiOperation(value = "회원 정보 조회", notes = "회원 아이디를 기반으로 회원 정보 조회", response = UserDto.class)
 	@ApiImplicitParams({
-		@ApiImplicitParam(name = "userId", value = "Kakao API에서 제공한 회원 아이디", required = true,
-			example = "1412733569"),
-		@ApiImplicitParam(name = "token", value = "회원 토큰", required = true)
+		// @ApiImplicitParam(name = "userId", value = "Kakao API에서 제공한 회원 아이디", required = true,
+		// 	example = "1412733569"),
+		// @ApiImplicitParam(name = "token", value = "회원 토큰", required = true)
 	})
 	@ApiResponses({
 		@ApiResponse(code = 200, message = "회원정보 조회 성공"),
@@ -85,9 +85,9 @@ public class UserController {
 	@ApiOperation(value = "회원 정보 수정", notes = "회원 아이디를 기반으로 회원 정보 수정 (닉네임만 가능)",
 		response = UserDto.class)
 	@ApiImplicitParams({
-		@ApiImplicitParam(name = "token", value = "회원 토큰", required = true),
-		@ApiImplicitParam(name = "user", value = "수정 할 유저 닉네임", required = true, dataTypeClass = UserDto.class),
-		@ApiImplicitParam(name = "userId", value = "회원 아이디", required = true, example = "1412733569")
+		// @ApiImplicitParam(name = "token", value = "회원 토큰", required = true),
+		// @ApiImplicitParam(name = "user", value = "수정 할 유저 닉네임", required = true, dataTypeClass = UserDto.class),
+		// @ApiImplicitParam(name = "userId", value = "회원 아이디", required = true, example = "1412733569")
 	})
 	@ApiResponses({
 		@ApiResponse(code = 201, message = "회원정보 수정 성공"),
@@ -106,10 +106,10 @@ public class UserController {
 	@RequestMapping(method = RequestMethod.DELETE, value = "/{userId}/{refreshToken}", produces = "application/json")
 	@ApiOperation(value = "회원 정보 삭제", notes = "회원 아이디를 기반으로 DB에서 삭제")
 	@ApiImplicitParams({
-		@ApiImplicitParam(name = "userId", value = "Kakao에서 제공해준 회원 아이디", required = true,
-			example = "1412733569"),
-		@ApiImplicitParam(name = "token", value = "회원 토큰", required = true),
-		@ApiImplicitParam(name = "refreshToken", value = "Kakao API에서 제공한 리프레시 토큰", required = true)
+		// @ApiImplicitParam(name = "userId", value = "Kakao에서 제공해준 회원 아이디", required = true,
+		// 	example = "1412733569"),
+		// @ApiImplicitParam(name = "token", value = "회원 토큰", required = true),
+		// @ApiImplicitParam(name = "refreshToken", value = "Kakao API에서 제공한 리프레시 토큰", required = true)
 	})
 	@ApiResponses({
 		@ApiResponse(code = 204, message = "회원정보 삭제 성공"),
@@ -128,8 +128,8 @@ public class UserController {
 	@RequestMapping(method = RequestMethod.POST, value = "/logout", produces = "application/json")
 	@ApiOperation(value = "로그아웃", notes = "Refresh 토큰을 기반으로 Kakao에 로그아웃 요청", response = Long.class)
 	@ApiImplicitParams({
-		@ApiImplicitParam(name = "refreshToken", value = "Kakao API에서 제공한 리프레시 토큰", required = true),
-		@ApiImplicitParam(name = "token", value = "회원 토큰", required = true)
+		// @ApiImplicitParam(name = "refreshToken", value = "Kakao API에서 제공한 리프레시 토큰", required = true),
+		// @ApiImplicitParam(name = "token", value = "회원 토큰", required = true)
 	})
 	@ApiResponses({
 		@ApiResponse(code = 200, message = "로그아웃 성공"),
