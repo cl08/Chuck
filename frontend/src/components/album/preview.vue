cd10fb61953b8f6300bb9e6946c852de5e541503
김유창 <oyes9316@gmail.com>
2020-10-29 18:37:54 +0900

    [feature] S03P31A206-145 : 인물 선택

--- frontend/src/components/album/preview.vue
+++ frontend/src/components/album/preview.vue
@@ -4,14 +4,11 @@
             <font size=4>미리보기</font>
         </div>
         <div class="dash" style="height:620px;">
-            <el-carousel indicator-position="none">
+            <!-- <el-carousel indicator-position="none">
                 <el-carousel-item v-for="(chuck, index) in getChuckList" :key="index">
                     <img :src="chuck.img[0]">
                 </el-carousel-item>
-            </el-carousel>
-            {{ getChuckList[0].title }}
-            
-            {{ getChuckList[0].content}}
+            </el-carousel> -->
         </div>
         <div class="dash">
             <font size=4>PDF로 내려받기</font>
