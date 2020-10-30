142611a85f74b402136390e7966d277e4b4c808f
average10@naver.com <average10@naver.com>
2020-10-30 17:36:36 +0900

    [feature] S03P31A206-165 : 동영상에 음악 입히기

--- backend/python/mkVideo.py
+++ backend/python/mkVideo.py
@@ -12,15 +12,14 @@ def main():
 
     # 동영상 제작
     paths = [os.path.join('dataset', path) for path in os.listdir('dataset')]
-    print(paths)
-    fps = 1
+    fps = 0.2
     frame_array = []
     for idx , path in enumerate(paths) : 
         img = cv2.imread(path)
-        print(img.shape)
+        # print(img.shape)
         # img.resize((500, 500, 3))
         # cv2.resize(img, (4000, 4000), interpolation=cv2.INTER_AREA)
-        img = cv2.resize(img, (500, 500)) 
+        img = cv2.resize(img, (500, 500))
         # plt.imshow(img)
         # plt.show()
         height, width, layers = img.shape
@@ -30,9 +29,27 @@ def main():
     print(len(frame_array))
     for i in range(len(frame_array)):
         out.write(frame_array[i])
+    
     out.release()
 
 
+    videoclip = VideoFileClip("method2.mp4")
+    audioclip = AudioFileClip("Fingertips.mp3")
+    # duration = videoclip.duration
+    # audioclip.duration = duration
+    audioclip.duration = videoclip.duration
+    videoclip.audio = audioclip
+    # print(videoclip.duration)
+    # videoclip.duration = duration
+    videoclip.write_videofile("new.mp4")
+
+
+    # audio = AudioFileClip("Fingertips.mp3")
+    # video = ImageClip('dataset/1.jpg', duration = audio.duration)
+    # video = video.set_audio(audio)
+    # video.write_videofile('test.mp4', fps = 24, codec="mpeg4")
+
+    # video = 
 
 
 
