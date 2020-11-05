1635dc49f16836a9339df8e29d66b3dd280ce7d7
kickrisk <dsghdh123@naver.com>
2020-11-05 15:46:11 +0900

    API 주소변경

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: 'http://k3a206.p.ssafy.io:8888/chuck/',
-  baseURL: 'http://localhost:8888/chuck/',
+  baseURL: 'http://k3a206.p.ssafy.io:8888/chuck/',
+  // baseURL: 'http://localhost:8888/chuck/',
   headers: {
     'Content-Type': 'application/json',
   },
