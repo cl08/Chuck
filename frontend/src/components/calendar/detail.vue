6acfe69c842feb0197887bed10ef4b20b50e819d
kickrisk <dsghdh123@naver.com>
2020-11-14 17:05:31 +0900

    [update] 삭제시 AI 데이터 재업

--- frontend/src/components/calendar/detail.vue
+++ frontend/src/components/calendar/detail.vue
@@ -133,9 +133,17 @@ export default {
                         token: this.$store.getters.getToken,
                     }
                 }).then(() => {
-                    this.$store.dispatch('delChuckList', {index: this.getSelectedDiary, id: this.getSelectedDiary})
-                    eventBus.$emit('updateList')
-                    eventBus.$emit('back')
+                    api.get(`pictures/gallery?groupId=${this.getSelectedGroup.id}`)
+                    .then(({ data }) => {
+                        this.$store.commit('setFaceDataGallery', data)
+                        api.get(`pictures/studio?groupId=${this.getSelectedGroup.id}`)
+                        .then(({ data }) => {
+                            this.$store.commit('setFaceDataStudio', data)
+                            this.$store.dispatch('delChuckList', {index: this.getSelectedDiary, id: this.getSelectedDiary})
+                            eventBus.$emit('updateList')
+                            eventBus.$emit('back')
+                        })
+                    })
                 })
             })
         },
