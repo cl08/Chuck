7f4cb4045a79fc9c9093f7afaa8bfbf20053ebf4
김유창 <oyes9316@gmail.com>
2020-11-15 17:27:15 +0900

    bugfix

--- frontend/src/components/gallery/personClassification.vue
+++ frontend/src/components/gallery/personClassification.vue
@@ -11,7 +11,7 @@
         <div class="underline" style="margin-top: -5px;"></div>
         </div>
         <div v-if="getFaceDataGallery.gallery_list" style="text-align:left; padding:20px 0px 10px 30px;">{{getFaceDataGallery.gallery_list.length}}명의 인물이 검색되었습니다.</div>
-        <div class="dash" style="width:550px; height:560px; text-align:left;">
+        <div v-if="getFaceDataGallery.gallery_list" class="dash" style="width:550px; height:560px; text-align:left;">
             <div v-if="getFaceDataGallery.gallery_list.length > 0" style="padding:8px;">
                 <span class="face pointer" @click="selectAll">ALL</span>
                 <span class="face pointer" v-for="(face, index) in getFaceDataGallery.gallery_list" :key="index" @click="select(index)" :style="'background-image:url(' + face.rep_image + ')'">
